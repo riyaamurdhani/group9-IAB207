fa7e0c45e7c48e300f7ead1a8aa9830d5ca12d5b
Karma Lhazom <karma.lhazom@connect.qut.edu.au>
2025-06-08 23:21:04 +1000

    updated main file

--- lab207_a3_grp_9/main.py
+++ lab207_a3_grp_9/main.py
@@ -1,9 +1,29 @@
 from app import create_app, db
-from flask import Flask
-from app.routes.index import register_routes
+from flask import Flask, session, render_template
+from flask_migrate import Migrate
 
 app = create_app()
-register_routes(app)
+
+migrate = Migrate(app, db)
+
+@app.route('/cause-error')
+def cause_error():
+    return 1 / 0  # This triggers ZeroDivisionError → 500
+    
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+@app.context_processor
+def inject_user():
+    return {
+        'user_name': session.get('user_name'),
+        'user_email': session.get('user_email')
+    }
 
 with app.app_context():
     db.create_all()
