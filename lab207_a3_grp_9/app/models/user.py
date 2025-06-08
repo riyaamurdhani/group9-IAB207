d7b26cb1e51100f2af734f2be3b7c11476e3d0ef
riyaamurdhani <n11921111@qut.edu.au>
2025-06-08 22:12:24 +1000

    Update user.py

--- lab207_a3_grp_9/app/models/user.py
+++ lab207_a3_grp_9/app/models/user.py
@@ -1,9 +1,11 @@
 from app import db, login_manager
 from flask_login import UserMixin
+from app.models.event import Event
+from app.models.comment import Comment
+from app.models.ticket import Ticket
 
 @login_manager.user_loader
 def load_user(user_id):
-    from app.models.user import User
     return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
@@ -16,3 +18,7 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(128), nullable=False)
     contact_number = db.Column(db.String(20), nullable=False)
     address = db.Column(db.String(255), nullable=False)
+
+    events = db.relationship('Event', backref='creator', lazy=True)
+    comments = db.relationship('Comment', backref='author', lazy=True)
+    tickets = db.relationship('Ticket', backref='buyer', lazy=True)
